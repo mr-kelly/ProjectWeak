db8a04d0018c457240b662c1207df2959cc921ae
Mrkelly <chepy.v@gmail.com>
2014-09-28 15:45:33 +0800

    math game

--- Assets/Code/CUIMathGame.cs
+++ Assets/Code/CUIMathGame.cs
@@ -17,7 +17,11 @@ public class CUIMathGame : CUIController
     public List<UIButton> SelectBtns;
     AudioSource TimeCounterSound;
     UILabel TimeCounterLabel;
+    float GameTime = 0;
+    
+    int Blood = 1; // 一滴血
 
+    int RightAnswerIndex = 0; // 正確答案按鈕的索引
     public override void OnInit()
     {
         base.OnInit();
@@ -67,11 +71,11 @@ public class CUIMathGame : CUIController
 
     IEnumerator TimerCounter()
     {
-        float time = 0;
+        GameTime = 0;
         while (true)
         {
-            time += Time.deltaTime;
-            TimeCounterLabel.text = string.Format("{0:F2}s", time);
+            GameTime += Time.deltaTime;
+            TimeCounterLabel.text = string.Format("{0:F2}s", GameTime);
             yield return null;
         }
         
@@ -79,6 +83,7 @@ public class CUIMathGame : CUIController
 
     void NewLevel()
     {
+        Blood = 1;
         NewQuestion();
         ResetAllTweens();
         StartCoroutine(TimerCounter());
@@ -91,22 +96,55 @@ public class CUIMathGame : CUIController
         if (selectIndex == RightAnswerIndex)
         {
             CBase.Log("Right");
+            Blood--;
         }
         else
         {
             CBase.Log("Wrong");
             CNGUIBridge.Instance.UiCamera.gameObject.transform.DOShakePosition(.5f, 20f);
         }
-        AnswerQuestion();
+
+        if (Blood <= 0)
+        {
+            OnGameOver();
+        }
+        else
+        {
+            NewQuestion();  // 有血，繼續
+        }
+        
     }
 
-    void AnswerQuestion()
+    /// <summary>
+    /// 受傷，屏幕變紅
+    /// </summary>
+    void OnRightAnswer()
     {
-        NewQuestion();
+        // 數字+1 彈過去
+        CUIMidMsg.QuickMsg("Right!");
+        PlayScoreAnimate(1);
     }
 
-    int RightAnswerIndex = 0;
+    void PlayScoreAnimate(int number)
+    {
+        GameObject scoreEff = GameObject.Instantiate(ScoreEffectTemplate.gameObject) as GameObject;
+        scoreEff.transform.position = Vector3.zero;
+        UILabel copyLabel = scoreEff.GetComponent<UILabel>();
+        copyLabel.text = number.ToString();
 
+        scoreEff.transform.DOMove(ScoreLabel.transform.position, 1f);
+    }
+
+    /// <summary>
+    /// 答對題目，出現的效果
+    /// </summary>
+    void OnWrongAnswer()
+    {
+        // 生命條減少, 無數字
+        CUIMidMsg.QuickMsg("Wrong!");
+        PlayScoreAnimate(-1);
+    }
+    
     void NewQuestion()
     {
         int rightAnswer = GenerateQuestion();
@@ -177,22 +215,23 @@ public class CUIMathGame : CUIController
         }
     }
 
-    /// <summary>
-    /// 受傷，屏幕變紅
-    /// </summary>
-    void ScreenHurt()
+    /// 遊戲結束
+    /// 錯誤就結束 (有生命）
+    void OnGameOver()
     {
-        // 生命條減少, 無數字
-        CUIMidMsg.QuickMsg("Wrong!");
-    }
+        CBase.Log("Time: {0}", GameTime);
 
-    /// <summary>
-    /// 答對題目，出現的效果
-    /// </summary>
-    void ScreenRight()
-    {
-        // 數字+1 彈過去
-        CUIMidMsg.QuickMsg("Right!");
+        float highestTime = 0;
+        if (PlayerPrefs.HasKey("HighestTime"))
+            highestTime = PlayerPrefs.GetFloat("HighestTime");
+
+        PlayerPrefs.SetFloat("HighestTime", GameTime);
+
+        int highestRight = 0;
+        if (PlayerPrefs.HasKey("HighestRight"))
+            highestRight = PlayerPrefs.GetInt("HighestRight");
+
+        PlayerPrefs.SetInt("HighestRight", highestRight);
     }
 
     // TODO: no use
